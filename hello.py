546da69f60a0cdd6ef75209407713681a29369c8
Ishaswork123 <164234645+Ishaswork123@users.noreply.github.com>
2024-12-05 16:42:42 +0100

    Create hello.py
    first-try

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Simple Login Program with No Input
+
+username = "admin"
+password = "1234"
+
+# Hardcoded check
+if username == "admin" and password == "1234":
+    print("Login successful! Welcome!")
+else:
+    print("Login failed! Incorrect username or password.")
